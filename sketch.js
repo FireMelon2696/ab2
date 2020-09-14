5e9de8bc914b7f5fd7a266e8558b29bd1d7b79e7
FireMelon2696 <69468124+FireMelon2696@users.noreply.github.com>
2020-09-14 19:27:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform;
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,9 +16,10 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-    platform = new Ground(150,305,300,170);
-    
-    ground = new Ground(600,height,1200,20)
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -33,16 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
-    chain = new Slingshot(bird.body,{x:200,y:100});
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -57,13 +60,23 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
     bird.display();
     platform.display();
-    chain.display();
+    //log6.display();
+    slingshot.display();    
 }
+
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
+
+
 function mouseReleased(){
-    chain.fly();
+    slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode==32){
+        slingshot.attach(bird.body);
+    }
 }
